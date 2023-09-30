b387dd00f19aef510962cf16167dd6d9c32c354a
Fidel Otieno <fidelotieno11@gmail.com>
2023-09-30 23:20:30 +0300

    Added the footer to link to my twitter

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,27 +1,42 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './main-page.css';
-import Header from './Header';
-import FeaturedHouse from './FeaturedHouse';
-import SearchResults from '../search-results';
-import HouseFilter from './HouseFilter';
-import HouseFromQuery from '../house/HouseFromQuery';
-import useHouses from '../hooks/useHouses';
-import useFeaturedHouse from '../hooks/useFeaturedHouse';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./main-page.css";
+import Header from "./Header";
+import FeaturedHouse from "./FeaturedHouse";
+import SearchResults from "../search-results";
+import HouseFilter from "./HouseFilter";
+import HouseFromQuery from "../house/HouseFromQuery";
+import useHouses from "../hooks/useHouses";
+import useFeaturedHouse from "../hooks/useFeaturedHouse";
 function App() {
   const allHouses = useHouses();
-  const featuredHouse= useFeaturedHouse(allHouses);
+  const featuredHouse = useFeaturedHouse(allHouses);
 
-  
   return (
     <Router>
       <div className="container">
         <Header subtitle="Providing houses all over the world" />
-        <HouseFilter allHouses={allHouses}/>
+        <HouseFilter allHouses={allHouses} />
         <Routes>
-          <Route path='/house/:id' element={<HouseFromQuery allHouses={allHouses}></HouseFromQuery>}></Route>
-          <Route path='/searchresults/:country' element={<SearchResults allHouses={allHouses}></SearchResults>} ></Route>
-          <Route exact path='/' element={<FeaturedHouse house={featuredHouse} />} />
+          <Route
+            path="/house/:id"
+            element={<HouseFromQuery allHouses={allHouses}></HouseFromQuery>}
+          ></Route>
+          <Route
+            path="/searchresults/:country"
+            element={<SearchResults allHouses={allHouses}></SearchResults>}
+          ></Route>
+          <Route
+            exact
+            path="/"
+            element={<FeaturedHouse house={featuredHouse} />}
+          />
         </Routes>
+        <footer>
+          Made with {"\u2764"} by ~
+          <a href="https://twitter.com/Fideltodayy" target="blank">
+            Fideltodayy
+          </a>
+        </footer>
       </div>
     </Router>
   );
